7c81bb40e73938a08390bef292ce4c53b1833d4e
cookjo <jordan.cook@pioneer.com>
2020-02-28 18:25:53 -0600

    Add taxa endpoints
    * Add support for array parameters as url-encoded, comma-separated lists

--- pyinaturalist/node_api.py
+++ pyinaturalist/node_api.py
@@ -1,16 +1,24 @@
 # Code to access the (read-only, but fast) Node based public iNaturalist API
 # See: http://api.inaturalist.org/v1/docs/
+from logging import getLogger
 from time import sleep
 from typing import Dict, Any, List
 
 import requests
 from urllib.parse import urljoin
 
-from pyinaturalist.constants import THROTTLING_DELAY, INAT_NODE_API_BASE_URL
+from pyinaturalist.constants import THROTTLING_DELAY, INAT_NODE_API_BASE_URL, RANKS
 from pyinaturalist.exceptions import ObservationNotFound
-from pyinaturalist.helpers import merge_two_dicts, get_user_agent
+from pyinaturalist.helpers import (
+    merge_two_dicts,
+    get_user_agent,
+    concat_list_params,
+    strip_empty_params,
+)
 
 PER_PAGE_RESULTS = 30  # Paginated queries: how many records do we ask per page?
+
+logger = getLogger(__name__)
 
 
 def make_inaturalist_api_get_call(
@@ -18,11 +26,13 @@ def make_inaturalist_api_get_call(
 ) -> requests.Response:
     """Make an API call to iNaturalist.
 
-    endpoint is a string such as 'observations' !! do not put / in front
+    endpoint is a string such as 'observations'
     method: 'GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE'
     kwargs are passed to requests.request
     Returns a requests.Response object
     """
+    params = strip_empty_params(params)
+    params = concat_list_params(params)
     headers = {"Accept": "application/json", "User-Agent": get_user_agent(user_agent)}
 
     response = requests.get(
@@ -94,3 +104,65 @@ def get_all_observations(params: Dict, user_agent: str = None) -> List[Dict[str,
 
         sleep(THROTTLING_DELAY)
         id_above = results[-1]["id"]
+
+
+def get_taxa_by_id(taxon_id, user_agent: str = None) -> Dict[str, Any]:
+    """Get one or more taxa by ID"""
+    if isinstance(taxon_id, list):
+        taxon_id = ",".join(taxon_id)
+
+    r = make_inaturalist_api_get_call(
+        "taxa/{}".format(taxon_id), {}, user_agent=user_agent
+    )
+    r.raise_for_status()
+    return r.json()
+
+
+def get_taxa(user_agent: str = None, **params) -> Dict[str, Any]:
+    """Given zero to many of following parameters, returns taxa matching the search criteria.
+    See https://api.inaturalist.org/v1/docs/#!/Taxa/get_taxa
+
+    :param q: Name must begin with this value
+    :param is_active: Taxon is active
+    :param taxon_id: Only show taxa with this ID, or its descendants
+    :param parent_id: Taxon's parent must have this ID
+    :param rank: Taxon must have this rank
+    :param rank_level: Taxon must have this rank level. Some example values are 70 (kingdom),
+        60 (phylum), 50 (class), 40 (order), 30 (family), 20 (genus), 10 (species), 5 (subspecies)
+    :param id_above: Must have an ID above this value
+    :param id_below: Must have an ID below this value
+    :param per_page: Number of results to return in a page. The maximum value is generally 200
+        unless otherwise noted
+    :param locale: Locale preference for taxon common names
+    :param preferred_place_id: Place preference for regional taxon common names
+    :param only_id: Return only the record IDs
+    :param all_names: Include all taxon names in the response
+
+    :returns: A list of dicts containing taxa results
+    """
+    r = make_inaturalist_api_get_call("taxa", params, user_agent=user_agent)
+    r.raise_for_status()
+    return r.json()
+
+
+def get_taxa_autocomplete(user_agent: str = None, **params) -> Dict[str, Any]:
+    """Given a query string, returns taxa with names starting with the search term
+
+    :param q: Name must begin with this value
+    :param is_active: Taxon is active
+    :param taxon_id: Only show taxa with this ID, or its descendants
+    :param rank: Taxon must have this rank
+    :param rank_level: Taxon must have this rank level. Some example values are 70 (kingdom),
+        60 (phylum), 50 (class), 40 (order), 30 (family), 20 (genus), 10 (species), 5 (subspecies)
+    :param per_page: Number of results to return in a page. The maximum value is generally 200 unless otherwise noted
+    :param locale: Locale preference for taxon common names
+    :param preferred_place_id: Place preference for regional taxon common names
+    :param all_names: Include all taxon names in the response
+
+    :returns: A list of dicts containing taxa results
+    """
+    r = make_inaturalist_api_get_call(
+        "taxa/autocomplete", params, user_agent=user_agent
+    )
+    r.raise_for_status()
+    return r.json()
