113ff2833f3f238d1dc6f8a26f9914251a1c3a86
Jordan Cook <jordan.cook@pioneer.com>
2020-02-28 18:25:53 -0600

    Add taxa endpoints
    * Add support for array parameters as url-encoded, comma-separated lists

--- pyinaturalist/__init__.py
+++ pyinaturalist/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+import logging
 
 __author__ = "Nicolas Noé"
 __email__ = "nicolas@niconoe.eu"
@@ -7,3 +7,6 @@ __version__ = "0.8.0"
 DEFAULT_USER_AGENT = "Pyinaturalist/{version}".format(version=__version__)
 
 user_agent = DEFAULT_USER_AGENT
+
+# Enable logging for urllib and other external loggers
+logging.basicConfig(level="DEBUG")
