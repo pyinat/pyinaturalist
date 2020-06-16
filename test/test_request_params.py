41c2cbe86ad525693bbf82728d9e10fdd051b2f5
Jordan Cook <jordan.cook@pioneer.com>
2020-06-16 12:19:18 -0500

    Drop support for python 3.4
    * Bump version number following release
    * Remove python 3.4 from tests and classifiers
    * Remove typing backport and use the stdlib version
    * Remove merge_two_dicts() and use {**dict, **dict} and other PEP 448 syntax
    * Use latest pytest (5.0+)
    * Use latest tox (3.15+)
    * Use json.JSONDecodeError
    * Remove other python 3.4-specific exceptions/workarounds
    * Remove requirements.txt; known minimum required package versions are all
      specified in setup.py now, and remaining version pins are not needed

--- test/test_request_params.py
+++ test/test_request_params.py
@@ -84,11 +84,10 @@ def test_preprocess_request_params(mock_bool, mock_datetime, mock_list, mock_str
 )
 @patch("pyinaturalist.response_format.as_geojson_feature")
 @patch("pyinaturalist.response_format._get_rank_range")
-@patch("pyinaturalist.node_api.merge_two_dicts")
 @patch("pyinaturalist.api_requests.preprocess_request_params")
 @patch("pyinaturalist.api_requests.requests.request")
 def test_all_node_requests_use_param_conversion(
-    request, preprocess_request_params, merge_two_dicts, get_rank_range, as_geojson, http_function,
+    request, preprocess_request_params, get_rank_range, as_geojson, http_function,
 ):
     request().json.return_value = {"total_results": 1, "results": [{}]}
     mock_args = get_mock_args_for_signature(http_function)
