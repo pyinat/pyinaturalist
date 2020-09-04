4b79603444579e49347a6c6f1b9e8501a58ffd60
Jordan Cook <jordan.cook@pioneer.com>
2020-09-04 16:39:01 -0500

    Make unit test sample data available to Sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,16 @@ from os import makedirs, symlink
 from os.path import abspath, dirname, exists, join
 
 DOCS_DIR = abspath(dirname(__file__))
-DOC_IMAGES_DIR = join(DOCS_DIR, "images")
 PROJECT_DIR = dirname(DOCS_DIR)
 PACKAGE_DIR = join(PROJECT_DIR, "pyinaturalist")
+
+# Source paths and symlink paths for static content to include
+SRC_IMAGE_DIR = join(DOCS_DIR, "images")
+SYMLINK_IMAGE_DIR = join(DOCS_DIR, "docs", "images")
+SRC_DATA_DIR = join(PROJECT_DIR, "test", "sample_data")
+SYMLINK_DATA_DIR = join(DOCS_DIR, "sample_data")
+
+# Add project path so we can import our package
 sys.path.insert(0, PROJECT_DIR)
 from pyinaturalist import __version__
 
@@ -17,11 +24,12 @@ needs_sphinx = "3.0"
 master_doc = "index"
 source_suffix = ".rst"
 version = release = __version__
+html_static_path = ["_static"]
+templates_path = ["_templates"]
+
 # Exclude the generated pyinaturalist.rst, which will just contain top-level __init__ info
 # and add an extra level to the toctree
 exclude_patterns = ["_build", "modules/pyinaturalist.rst"]
-html_static_path = ["_static"]
-templates_path = ["_templates"]
 
 # Sphinx extension modules
 extensions = [
@@ -69,9 +77,11 @@ html_theme = "sphinx_rtd_theme"
 
 
 def setup(app):
-    """Run some additional steps after the Sphinx builder is intialized. This allows us to
-    run any custom behavior that would otherwise go in the Makefile, so the readthedocs builder
-    will behave the same as building the docs manually.
+    """Run some additional steps after the Sphinx builder is intialized.
+
+    In addition to basic things like adding static resources, this allows us to run any custom
+    behavior that would otherwise go in the Makefile, so the readthedocs builder will behave the
+    same as building the docs manually.
 
     Reference:
         * https://www.sphinx-doc.org/en/master/extdev/appapi.html#sphinx-core-events
@@ -79,15 +89,21 @@ def setup(app):
         * https://github.com/sphinx-contrib/apidoc
     """
     app.connect("builder-inited", make_symlinks)
+    app.add_css_file("collapsible_container.css")
 
 
 def make_symlinks(app):
     """Create symlinks so that relative links to static content will resolve correctly in both:
     * README.rst (as displayed on GitHub and PyPi) and
     * Sphinx html docs (as displayed on readthedocs.io)
+
+    Also symlink sample response data so it can be used by both Sphinx and pytest
     """
-    doc_symlinks_dir = join(DOCS_DIR, "docs")
-    symlinked_images_dir = join(doc_symlinks_dir, "images")
-    makedirs(doc_symlinks_dir, exist_ok=True)
-    if not exists(symlinked_images_dir):
-        symlink(DOC_IMAGES_DIR, symlinked_images_dir)
+    make_symlink(SRC_IMAGE_DIR, SYMLINK_IMAGE_DIR)
+    make_symlink(SRC_DATA_DIR, SYMLINK_DATA_DIR)
+
+
+def make_symlink(src, dest):
+    makedirs(dirname(dest), exist_ok=True)
+    if not exists(dest):
+        symlink(src, dest)
