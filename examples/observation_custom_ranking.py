ffd1322ddcf2f70a17a10341088b81b6cb71cbbb
Jordan Cook <jordan.cook@pioneer.com>
2021-02-20 18:50:48 -0600

    Use a dataframe to normalize ranking values and do most of the other number-crunching

--- examples/observation_custom_ranking.py
+++ examples/observation_custom_ranking.py
@@ -17,15 +17,16 @@ import requests_cache
 from pyinaturalist.node_api import get_identifications, get_observations
 from pyinaturalist.response_format import to_dataframe
 
-# fmt: off
-# TODO: Normalize values before applying weights
 ICONIC_TAXON = 'Arachnida'
+
+# Note: all values are normalized before ranking weights are applied.
+# fmt: off
 RANKING_WEIGHTS = {
     'iconic_taxon_rg_observations_count': 1.5,   # Number of research-grade observations for ICONIC_TAXON
     'iconic_taxon_identifications_count': 2.0,   # Number of identifications for ICONIC_TAXON
-    'observations_count':                 0.05,  # Total observations (all taxa)
-    'identifications_count':              0.05,  # Total identifications (all taxa)
-    'account_age_days':                   0.8,   # Age of user account, in days
+    'observations_count':                 0.1,   # Total observations (all taxa)
+    'identifications_count':              0.1,   # Total identifications (all taxa)
+    'account_age_days':                   0.5,   # Age of user account, in days
 }
 
 requests_cache.install_cache(backend='sqlite', cache_name='inat_requests.db')
@@ -42,6 +43,7 @@ def append_user_stats(observations):
         f'Getting stats for {len(user_stats)} unique observers of '
         f'{len(observations)} total observations'
     )
+    logger.warning(f'Estimated time, with conservative API request throttling: {len(user_stats)/60} minutes')
     for user_id, user_info in user_stats.items():
         logger.debug(f'Getting stats for user {user_id}')
         user_obs = get_observations(
@@ -55,7 +57,7 @@ def append_user_stats(observations):
 
         user_stats[user_id]['iconic_taxon_rg_observations_count'] = user_obs['total_results']
         user_stats[user_id]['iconic_taxon_identifications_count'] = user_ids['total_results']
-        user_stats[user_id]['account_age_days'] = user_age.days
+        user_stats[user_id]['account_age_days'] = max(user_age.days, 1)
         sleep(1)
 
     # Append user stats to the observation records
@@ -64,14 +66,21 @@ def append_user_stats(observations):
     return observations
 
 
-def get_observation_rank(observation):
-    return sum([observation['user'][key] * RANKING_WEIGHTS[key] for key in RANKING_WEIGHTS])
+def rank_observations(df):
+    """Combine normalized and weighted values into a single ranking value, and sort"""
+    def normalize(series):
+        return (series - series.mean()) / series.std()
+
+    df['rank'] = sum([normalize(df[key]) * weight for key, weight in RANKING_WEIGHTS.items()])
+    return df.sort_values('rank')
+
 
 
 def main():
     response = get_observations(iconic_taxa=ICONIC_TAXON, quality_grade='needs_id', per_page=200)
     observations = append_user_stats(response['results'])
-    observations = sorted(observations, key=get_observation_rank)
+    df = to_dataframe(observations)
+    df = rank_observations(df)
     return observations
 
 
