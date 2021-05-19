db18ea2439e835ad9e6ce0508c765b48b6c468cd
Jordan Cook <jordan.cook@pioneer.com>
2021-05-18 19:56:29 -0500

    Add Place model

---
+++ pyinaturalist/models/place.py
@@ -0,0 +1,25 @@
+from typing import Dict, List
+
+import attr
+
+from pyinaturalist.constants import Coordinates
+from pyinaturalist.models import BaseModel, coordinate_pair, kwarg
+
+# TODO: Make a separate model for geojson type? or optionally use `geojson` library?
+GeoJson = Dict
+
+
+@attr.s
+class Place(BaseModel):
+    admin_level: int = kwarg
+    ancestor_place_ids: List[str] = attr.ib(factory=list)
+    bbox_area: float = kwarg
+    bounding_box_geojson: GeoJson = attr.ib(factory=dict)
+    category: str = kwarg  # Either 'standard' or 'community'
+    display_name: str = kwarg
+    geometry_geojson: GeoJson = attr.ib(factory=dict)
+    id: int = kwarg
+    location: Coordinates = coordinate_pair
+    name: str = kwarg
+    place_type: int = kwarg
+    slug: str = kwarg
