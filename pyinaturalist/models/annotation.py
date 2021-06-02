4fd389cc2d85297dc9a2ea6f02248a020e2be04b
Jordan Cook <jordan.cook@pioneer.com>
2021-06-01 20:33:36 -0500

    Add Annotation model

---
+++ pyinaturalist/models/annotation.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from attr import field
+
+from pyinaturalist.models import BaseModel, LazyProperty, User, define_model, kwarg
+
+
+@define_model
+class Annotation(BaseModel):
+    """A dataclass containing information about an annotation, matching the schema of annotations
+    from `GET /observations <https://api.inaturalist.org/v1/docs/#!/Observations/get_observations>`_.
+    """
+
+    concatenated_attr_val: str = kwarg
+    controlled_attribute_id: int = kwarg
+    controlled_value_id: int = kwarg
+    user_id: int = kwarg
+    uuid: str = kwarg
+    vote_score: int = kwarg
+    votes: List = field(factory=list)
+    user: property = LazyProperty(User.from_json)
+
+    @property
+    def values(self) -> List[str]:
+        """Split pipe-delimited annotation values into separate tokens"""
+        if not self.concatenated_attr_val:
+            return []
+        return self.concatenated_attr_val.split('|')
