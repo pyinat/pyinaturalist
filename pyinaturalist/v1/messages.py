0d5af49c06ba6ee9b0ea3c46a632f01d517d999a
Jordan Cook <jordan.cook@pioneer.com>
2021-12-08 14:44:11 -0600

    Add GET messages endpoints

---
+++ pyinaturalist/v1/messages.py
@@ -0,0 +1,91 @@
+from logging import getLogger
+
+from pyinaturalist.constants import JsonResponse, MultiInt
+from pyinaturalist.converters import convert_all_timestamps
+from pyinaturalist.docs import document_request_params
+from pyinaturalist.docs import templates as docs
+from pyinaturalist.v1 import get_v1
+
+logger = getLogger(__name__)
+
+
+@document_request_params(docs._message_id, docs._access_token)
+def get_message_by_id(message_id: MultiInt, **params) -> JsonResponse:
+    """Get a message by ID
+
+    .. rubric:: Notes
+
+    * :fa:`lock` :ref:`Requires authentication <auth>`
+    * API reference: :v1:`GET /messages/{id} <https://api.inaturalist.org/v1/docs/#!/Messages/get_messages_id>`
+
+    Example:
+        >>> response = get_messages(123456)
+        >>> pprint(response)
+
+        .. admonition:: Example Response
+            :class: toggle
+
+            .. literalinclude:: ../sample_data/get_messages.json
+
+    Returns:
+        Response dict containing user record
+    """
+    response = get_v1('messages', ids=message_id, **params)
+    messages = response.json()
+    messages['results'] = convert_all_timestamps(messages['results'])
+    return messages
+
+
+@document_request_params(docs._message_params, docs._access_token)
+def get_messages(**params) -> JsonResponse:
+    """Get messages from the user's inbox
+
+    .. rubric:: Notes
+
+    * :fa:`lock` :ref:`Requires authentication <auth>`
+    * API reference: :v1:`GET /messages <https://api.inaturalist.org/v1/docs/#!/Messages/get_messages>`
+
+    Example:
+        >>> response = get_messages()
+        >>> pprint(response)
+
+        .. admonition:: Example Response
+            :class: toggle
+
+            .. literalinclude:: ../sample_data/get_messages.json
+
+    Returns:
+        Response dict containing user record
+    """
+    # `threads` is not compatible with `q` param, and includes totals from both inbox and sent
+    if params.get('threads') is True:
+        params['box'] = 'any'
+        params['q'] = None
+
+    response = get_v1('messages', **params)
+    messages = response.json()
+    messages['results'] = convert_all_timestamps(messages['results'])
+    return messages
+
+
+def get_unread_meassage_count(**params) -> int:
+    """Get the number of unread messages in the user's inbox
+
+    .. rubric:: Notes
+
+    * :fa:`lock` :ref:`Requires authentication <auth>`
+    * API reference: :v1:`GET /messages/unread <https://api.inaturalist.org/v1/docs/#!/Messages/get_messages_unread>`
+
+    Example:
+        >>> get_unread_meassage_count()
+        12
+
+    Returns:
+        Unread message count
+    """
+    response = get_v1('messages/unread', **params)
+    try:
+        return int(response.json()['count'])
+    except (KeyError, TypeError, ValueError):
+        logger.error(f'Failed to get unread message count: {response.text}', exc_info=True)
+        return 0
---
+++ test/v1/test_messages.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+from dateutil.tz import tzoffset
+
+from pyinaturalist.constants import API_V1_BASE_URL
+from pyinaturalist.v1 import get_message_by_id, get_messages, get_unread_meassage_count
+from test.sample_data import SAMPLE_DATA
+
+
+def test_get_message_by_id(requests_mock):
+    message_id = 12345
+    requests_mock.get(
+        f'{API_V1_BASE_URL}/messages/{message_id}',
+        json=SAMPLE_DATA['get_messages'],
+        status_code=200,
+    )
+
+    message = get_message_by_id(message_id)['results'][0]
+    assert message['id'] == message_id
+    assert message['created_at'] == datetime(
+        2019, 9, 2, 18, 50, 24, 651000, tzinfo=tzoffset(None, -18000)
+    )
+    assert message['from_user']['id'] == 12345
+    assert message['to_user']['id'] == 2115051
+
+
+def test_get_messages(requests_mock):
+    requests_mock.get(
+        f'{API_V1_BASE_URL}/messages',
+        json=SAMPLE_DATA['get_messages'],
+        status_code=200,
+    )
+
+    message = get_messages()['results'][0]
+    assert message['id'] == 12345
+    assert message['created_at'] == datetime(
+        2019, 9, 2, 18, 50, 24, 651000, tzinfo=tzoffset(None, -18000)
+    )
+    assert message['from_user']['id'] == 12345
+    assert message['to_user']['id'] == 2115051
+
+
+def test_get_messages__threads(requests_mock):
+    requests_mock.get(
+        f'{API_V1_BASE_URL}/messages',
+        json=SAMPLE_DATA['get_messages'],
+        status_code=200,
+    )
+
+    message = get_messages(threads=True)['results'][0]
+    assert message['id'] == 12345
+
+
+def test_get_unread_meassage_count(requests_mock):
+    requests_mock.get(
+        f'{API_V1_BASE_URL}/messages/unread',
+        json={'count': 12},
+        status_code=200,
+    )
+
+    assert get_unread_meassage_count() == 12
+
+
+def test_get_unread_meassage_count__invalid(requests_mock):
+    requests_mock.get(
+        f'{API_V1_BASE_URL}/messages/unread',
+        json={'results': 'invalid'},
+        status_code=200,
+    )
+
+    assert get_unread_meassage_count() == 0
