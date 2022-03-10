d5d4bd5af6c6f1c5a4a2e54dcd136774a078563b
Jordan Cook <jordan.cook@pioneer.com>
2022-03-09 20:35:35 -0600

    Add unit tests

--- test/v1/test_projects.py
+++ test/v1/test_projects.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from unittest.mock import patch
 
 from dateutil.tz import tzutc
 
@@ -8,14 +9,15 @@ from pyinaturalist.v1 import (
     delete_project_observation,
     get_projects,
     get_projects_by_id,
+    update_project,
 )
-from test.conftest import load_sample_data
+from test.sample_data import SAMPLE_DATA
 
 
 def test_get_projects(requests_mock):
     requests_mock.get(
         f'{API_V1_BASE_URL}/projects',
-        json=load_sample_data('get_projects.json'),
+        json=SAMPLE_DATA['get_projects'],
         status_code=200,
     )
 
@@ -34,7 +36,7 @@ def test_get_projects(requests_mock):
 def test_get_projects_by_id(requests_mock):
     requests_mock.get(
         f'{API_V1_BASE_URL}/projects/8348,6432',
-        json=load_sample_data('get_projects_by_id.json'),
+        json=SAMPLE_DATA['get_projects_by_id'],
         status_code=200,
     )
     response = get_projects_by_id([8348, 6432])
@@ -52,7 +54,7 @@ def test_get_projects_by_id(requests_mock):
 def test_add_project_observation(requests_mock):
     requests_mock.post(
         f'{API_V1_BASE_URL}/project_observations',
-        json=load_sample_data('add_project_observation.json'),
+        json=SAMPLE_DATA['add_project_observation'],
         status_code=200,
     )
     response = add_project_observation(project_id=1234, observation_id=5678, access_token='token')
@@ -68,3 +70,29 @@ def test_delete_project_observation(requests_mock):
         project_id=1234, observation_id=5678, access_token='token'
     )
     assert response.status_code == 200
+
+
+@patch('pyinaturalist.v1.projects.put_v1')
+def test_update_project(mock_put):
+    update_project(1234, title='New Title', description='New Description')
+
+    request_args = mock_put.call_args[1]
+    project_params = request_args['json']['project']
+    assert request_args['timeout'] == 60
+    assert project_params['title'] == 'New Title'
+    assert project_params['description'] == 'New Description'
+
+
+@patch('pyinaturalist.v1.projects.put_v1')
+def test_update_project__remove_users(mock_put, requests_mock):
+    requests_mock.get(
+        f'{API_V1_BASE_URL}/projects/1234',
+        json=SAMPLE_DATA['get_projects'],
+        status_code=200,
+    )
+    update_project(1234, remove_users=[5678])
+
+    project_params = mock_put.call_args[1]['json']['project']
+    rules = project_params['project_observation_rules_attributes']
+    assert rules[0]['id'] == 1234 and not rules[0].get('_destroy')
+    assert rules[1]['id'] == 5678 and rules[1]['_destroy'] is True
