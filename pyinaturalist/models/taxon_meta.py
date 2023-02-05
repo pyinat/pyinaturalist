67dd0701d2ca2b347e3c2373f23e2cf4d14a2800
Jordan Cook <jordan.cook.git@proton.me>
2023-02-05 11:53:17 -0600

    WIP: Map conservation status codes to full names

--- pyinaturalist/models/taxon_meta.py
+++ pyinaturalist/models/taxon_meta.py
@@ -1,8 +1,11 @@
 """Models for additional endpoint-specific metadata associated with a taxon"""
+import re
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, List, Optional
 
-from pyinaturalist.constants import CONSERVATION_STATUSES, ESTABLISTMENT_MEANS, DateTime, TableRow
+from attr import define
+
+from pyinaturalist.constants import ESTABLISTMENT_MEANS, DateTime, TableRow
 from pyinaturalist.models import (
     BaseModel,
     LazyProperty,
@@ -72,14 +75,9 @@ class ConservationStatus(IdWrapperMixin, BaseModel):
         default=None,
         doc='Default geoprivacy level; may be obscured or private for protected species',
     )
-    iucn: int = field(default=None, doc='IUCD ID, if applicable')
+    iucn: int = field(default=None, doc='IUCN ID, if applicable')
     source_id: int = field(default=None)
-    status: str = field(
-        default=None,
-        converter=upper,
-        options=CONSERVATION_STATUSES,
-        doc='Short code for conservation status',
-    )
+    status: str = field(default=None, converter=upper, doc='Short code for conservation status')
     status_name: str = field(default=None, doc='Full name of conservation status')
     taxon_id: int = field(default=None, doc='Taxon ID')
     updated_at: DateTime = datetime_field(doc='Date and time the record was last updated')
@@ -95,6 +93,10 @@ class ConservationStatus(IdWrapperMixin, BaseModel):
     # TODO: Are these needed? They appear to be redundant with `status` and `status_name`
     # iucn_status: str = field(default=None)
     # iucn_status_code: str = field(default=None)
+
+    @property
+    def full_name(self) -> str:
+        return f'{self.status_name} ({self.status}) in {self.place.name}'
 
     @property
     def _str_attrs(self) -> List[str]:
@@ -224,3 +226,123 @@ class TaxonSummary(BaseModel):
     @property
     def _str_attrs(self) -> List[str]:
         return ['conservation_status', 'listed_taxon']
+
+
+@define
+class IUCNStatus:
+    id: int = field()
+    code: str = field()
+    description: str = field()
+
+
+# IUCN Conservation status codes from https://www.iucnredlist.org
+IUCN_STATUSES = [
+    IUCNStatus(0, 'NE', 'not evaluated'),
+    IUCNStatus(5, 'DD', 'data deficient'),
+    IUCNStatus(10, 'LC', 'least concern'),
+    IUCNStatus(20, 'NT', 'near threatened'),
+    IUCNStatus(30, 'VU', 'vulnerable'),
+    IUCNStatus(40, 'EN', 'endangered'),
+    IUCNStatus(50, 'CR', 'critically endangered'),
+    IUCNStatus(60, 'EW', 'extinct in the wild'),
+    IUCNStatus(70, 'EX', 'extinct'),
+]
+IUCN_STATUSES_BY_CODE = {s.code: s.description for s in IUCN_STATUSES}
+IUCN_STATUSES_BY_ID = {s.id: s.description for s in IUCN_STATUSES}
+
+# Levels may be in ranges, and prefixed with a regional level, e.g. S2S3
+# https://www.natureserve.org/nsexplorer/about-the-data/statuses/conservation-status-categories
+NATURESERVE_STATUS_CODES = {
+    'X': 'extinct',
+    'H': 'possibly extinct',
+    '1': 'critically imperiled',
+    '2': 'imperiled',
+    '3': 'vulnerable',
+    '4': 'apparently secure',
+    '5': 'secure',
+}
+
+# https://www.dof.gob.mx/normasOficiales/4254/semarnat/semarnat.htm
+NORMA_OFICIAL_059_STATUS_CODES = {
+    'P': 'en peligro de extinción',
+    'A': 'amenazada',
+    'PR': 'sujeta a protección especial',
+    'EX': 'probablemente extinta en el medio silvestre',
+}
+
+GENERIC_STATUS_CODES = {
+    'SE': 'endangered',
+    'FE': 'endangered',
+    'LE': 'endangered',
+    'E': 'endangered',
+    'ST': 'threatened',
+    'FT': 'threatened',
+    'LT': 'threatened',
+    'T': 'threatened',
+    'LC': 'least concern',
+    'SC': 'special concern',
+    'C': 'candidate',
+}
+
+#  Alternatively
+# GENERIC_STATUS_CODES = {
+#     'endangered': ['se', 'fe', 'le', 'e'],
+#     'threatened': ['st', 'ft', 'lt', 't'],
+#     'least concern': ['lc'],
+#     'special concern': ['sc'],
+#     'candidate': ['c'],
+# }
+
+
+def translate_status_code(status: str, iucn_id: int = -1, authority: str = '') -> str:
+    """Translate a conservation status code from a given authority into a descriptive name"""
+    status = status.upper()
+    authority = authority.lower()
+
+    if authority == 'iucn':
+        return IUCN_STATUSES_BY_CODE[status]
+    elif authority == 'natureserve':
+        return translate_natureserve_code(status)
+    elif authority == 'norma_oficial_059':
+        return NORMA_OFICIAL_059_STATUS_CODES[status]
+    elif status in GENERIC_STATUS_CODES:
+        return GENERIC_STATUS_CODES[status]
+    elif iucn_id in IUCN_STATUSES_BY_ID:
+        return IUCN_STATUSES_BY_ID[iucn_id]
+    else:
+        raise ValueError(
+            f'Could not parse conservation status code: {status} From authority: {authority}'
+        )
+
+
+def translate_natureserve_code(status: str) -> str:
+    """Translate a NatureServe status code to a status name, including level ranges"""
+    if status in NATURESERVE_STATUS_CODES:
+        return NATURESERVE_STATUS_CODES[status]
+
+    match = re.match(r'\D+([\dXH])(?:\D+([\dXH]))?', status)
+    if not match:
+        raise ValueError(f'Could not parse NatureServe status code: {status}')
+
+    levels = [int(lvl) for lvl in match.groups() if lvl]
+    # Single level
+    if len(levels) == 1:
+        level = levels[0]
+    # Level range spanning 3 levels; pick average
+    elif levels[1] - levels[0] == 2:
+        level = levels[0] + 1
+    # Level range spanning 2 levels; pick higher
+    elif levels[1] - levels[0] == 1:
+        level = levels[0]
+    else:
+        print(levels)
+        print(levels[1] - levels[0])
+        raise ValueError(f'Could not parse NatureServe level range: {status}')
+
+    return NATURESERVE_STATUS_CODES[str(level)]
+
+
+assert translate_natureserve_code('S2') == 'imperiled'
+assert translate_natureserve_code('G2') == 'imperiled'
+assert translate_natureserve_code('S2S3B') == 'imperiled'
+assert translate_natureserve_code('N2N4') == 'vulnerable'
