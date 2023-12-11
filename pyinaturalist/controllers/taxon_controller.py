2d193252fe6711607d26a1cc937690d0b773b3b0
Jordan Cook <jordan.cook.git@proton.me>
2023-12-10 22:39:21 -0600

    Similar refactoring for annotation, place, project, taxon, and user controllers

--- pyinaturalist/controllers/taxon_controller.py
+++ pyinaturalist/controllers/taxon_controller.py
@@ -5,7 +5,8 @@ from attr import fields_dict
 from pyinaturalist.constants import MultiInt
 from pyinaturalist.controllers import BaseController
 from pyinaturalist.converters import ensure_list
-from pyinaturalist.docs import document_controller_params
+from pyinaturalist.docs import copy_doc_signature
+from pyinaturalist.docs import templates as docs
 from pyinaturalist.models import Taxon
 from pyinaturalist.paginator import IDPaginator, Paginator
 from pyinaturalist.v1 import get_taxa, get_taxa_autocomplete, get_taxa_by_id
@@ -17,30 +18,71 @@ class TaxonController(BaseController):
     """:fa:`dove` Controller for Taxon requests"""
 
     def __call__(self, taxon_id: int, **kwargs) -> Optional[Taxon]:
-        """Get a single taxon by ID"""
+        """Get a single taxon by ID
+
+        Example:
+            >>> client.taxa(343248)
+
+        Args:
+            taxon_id: A single taxon ID
+            locale: Locale preference for taxon common names
+            preferred_place_id: Place preference for regional taxon common names
+            all_names: Include all taxon names in the response
+        """
         return self.from_ids(taxon_id, **kwargs).one()
 
     def from_ids(self, taxon_ids: MultiInt, **params) -> Paginator[Taxon]:
-        """Get taxa by ID
+        """Get one or more taxa by ID
+
+        .. rubric:: Notes
+
+        * API reference: :v1:`GET /taxa/{id} <Taxa/get_taxa_id>`
+
+
+        Example:
+            >>> client.get_taxa_by_id([3, 343248])
 
         Args:
             taxon_ids: One or more taxon IDs
+            locale: Locale preference for taxon common names
+            preferred_place_id: Place preference for regional taxon common names
+            all_names: Include all taxon names in the response
         """
-        return self.client.paginate(
+
+        params = self.client.add_defaults(get_taxa_by_id, params)
+        return IDPaginator(
             get_taxa_by_id,
             Taxon,
-            cls=IDPaginator,
             ids=ensure_list(taxon_ids),
             ids_per_request=IDS_PER_REQUEST,
-            **params
+            **params,
         )
 
-    @document_controller_params(get_taxa_autocomplete)
+    @copy_doc_signature(docs._taxon_params)
     def autocomplete(self, **params) -> Paginator[Taxon]:
+        """Given a query string, return taxa with names starting with the search term
+
+        .. rubric:: Notes
+
+        * API reference: :v1:`GET /taxa/autocomplete <Taxa/get_taxa_autocomplete>`
+        * There appears to currently be a bug in the API that causes ``per_page`` to not have any effect.
+
+        Example:
+            >>> client.taxa.autocomplete(q='vespi')
+        """
         return self.client.paginate(get_taxa_autocomplete, Taxon, **params)
 
-    @document_controller_params(get_taxa)
+    @copy_doc_signature(docs._taxon_params, docs._taxon_id_params)
     def search(self, **params) -> Paginator[Taxon]:
+        """Search taxa
+
+        .. rubric:: Notes
+
+        * API reference: :v1:`GET /taxa <Taxa/get_taxa>`
+
+        Example:
+            >>> client.taxa.search(q='vespi', rank=['genus', 'family'])
+        """
         return self.client.paginate(get_taxa, Taxon, **params)
 
     def populate(self, taxon: Taxon, **params) -> Taxon:
--- pyinaturalist/controllers/user_controller.py
+++ pyinaturalist/controllers/user_controller.py
@@ -1,9 +1,8 @@
-from typing import List, Optional
+from typing import Optional
 
-from pyinaturalist.constants import MultiIntOrStr
+from pyinaturalist.constants import IntOrStr, MultiIntOrStr
 from pyinaturalist.controllers import BaseController
 from pyinaturalist.converters import ensure_list
-from pyinaturalist.docs import document_controller_params
 from pyinaturalist.models import User
 from pyinaturalist.paginator import IDPaginator, Paginator
 from pyinaturalist.v1 import get_current_user, get_user_by_id, get_users_autocomplete
@@ -12,8 +11,15 @@ from pyinaturalist.v1 import get_current_user, get_user_by_id, get_users_autocom
 class UserController(BaseController):
     """:fa:`user` Controller for User requests"""
 
-    def __call__(self, user_id, **kwargs) -> Optional[User]:
-        """Get a single user by ID"""
+    def __call__(self, user_id: IntOrStr, **kwargs) -> Optional[User]:
+        """Get a single user by ID
+
+        Example:
+            >>> user = client.users(1)
+
+        Args:
+            user_id: A single user ID
+        """
         return self.from_ids(user_id, **kwargs).one()
 
     def from_ids(self, user_ids: MultiIntOrStr, **params) -> Paginator[User]:
@@ -26,23 +32,45 @@ class UserController(BaseController):
 
             Get multiple users by ID:
 
-            >>> users = client.users.from_id(1).all()
+            >>> users = client.users.from_id([1,2]).all()
 
         Args:
-            user_ids: One or more project IDs
+            user_ids: One or more user IDs
         """
         return self.client.paginate(
             get_user_by_id, User, cls=IDPaginator, ids=ensure_list(user_ids), **params
         )
 
-    # TODO: Wrap in paginator?
-    @document_controller_params(get_users_autocomplete)
-    def autocomplete(self, **params) -> List[User]:
-        response = self.client.request(get_users_autocomplete, **params)
-        return User.from_json_list(response)
+    def autocomplete(
+        self, q: Optional[str] = None, project_id: Optional[int] = None, **params
+    ) -> Paginator[User]:
+        """Given a query string, return users with names or logins starting with the search term
 
-    @document_controller_params(get_current_user)
+        .. rubric:: Notes
+
+        * API reference: :v1:`GET /users/autocomplete <Users/get_users_autocomplete>`
+
+        Example:
+            >>> client.users.autocomplete(q='my_userna')
+
+        Args:
+            q: Search query
+            project_id: Only show users who are members of this project
+        """
+        return self.client.paginate(
+            get_users_autocomplete, User, q=q, project_id=project_id, **params
+        )
+
     def me(self, **params) -> User:
+        """Get your own user profile
+
+        .. rubric:: Notes
+
+        * :fa:`lock` :ref:`Requires authentication <auth>`
+        * API reference: :v1:`GET /users/me <Users/get_users_me>`
+
+        Example:
+            >>> client.users.me()
+        """
         response = self.client.request(get_current_user, auth=True, **params)
-        print(response)
         return User.from_json(response)
