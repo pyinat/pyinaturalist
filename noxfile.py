9525fe3c83772b15e2f6c34ed30e8efc61795b0e
Jordan Cook <jordan.cook.git@proton.me>
2024-08-20 20:42:40 -0500

    Convert project & packaging config to uv

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,6 @@ from os.path import join
 from shutil import rmtree
 
 import nox
-from nox_poetry import session
 
 nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ['lint', 'cov']
@@ -25,7 +24,7 @@ CLEAN_DIRS = [
 ]
 
 
-@session(python=['3.8', '3.9', '3.10', '3.11', '3.12', '3.13'])
+@nox.session(python=['3.8', '3.9', '3.10', '3.11', '3.12', '3.13'])
 def test(session):
     """Run tests for a specific python version"""
     test_paths = session.posargs or ['test']
@@ -33,7 +32,7 @@ def test(session):
     session.run('pytest', '-n', 'auto', *test_paths)
 
 
-@session(python=False)
+@nox.session(python=False)
 def clean(session):
     """Clean up temporary build + documentation files"""
     for dir in CLEAN_DIRS:
@@ -41,22 +40,21 @@ def clean(session):
         rmtree(dir, ignore_errors=True)
 
 
-@session(python=False)
-@session(python=False, name='cov')
+@nox.session(python=False, name='cov')
 def coverage(session):
     """Run tests and generate coverage report"""
     cmd = 'pytest -n auto --cov --cov-report=term --cov-report=html'
     session.run(*cmd.split(' '))
 
 
-@session(python=False)
+@nox.session(python=False)
 def docs(session):
     """Build Sphinx documentation"""
     cmd = 'sphinx-build docs docs/_build/html -j auto'
     session.run(*cmd.split(' '))
 
 
-@session(python=False)
+@nox.session(python=False)
 def livedocs(session):
     """Auto-build docs with live reload in browser.
     Add `-- open` to also open the browser after starting.
@@ -73,7 +71,7 @@ def livedocs(session):
     session.run(*cmd.split(' '))
 
 
-@session(python=False)
+@nox.session(python=False)
 def lint(session):
     """Run linters and code formatters via pre-commit"""
     cmd = 'pre-commit run --all-files'
